3fcc8e9d9b0c0d39af78d51e31d1078a46d651e6
paulparkinson559 <141360403+paulparkinson559@users.noreply.github.com>
2024-06-08 04:22:52 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+root = Tk ()
+
+myLabel= Label(root, text='hello world'
+                          'jhgjhgggjhg'
+                          'lhjjhjkjkhj'
+                          ';jkkjkjljll'
+                          'ljhjhjkhkhkj'
+                          'lkjhjhjkhkjj')
+myLabel.pack()
+root.mainloop()
+
